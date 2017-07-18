da63e89ad3a56aa0780bbc88823d8095839bf3e3
amashoshyna <a.mashoshyna@gmail.com>
2017-07-18 08:17:37 +0300

    generator

--- server.js
+++ server.js
@@ -21,21 +21,40 @@ const commitFilter = commit => ["added", "removed", "modified"]
 
 function webhook(request, response) {
   request.on("data", data => {
-const commitsWithMetaData =JSON.parse(atob(data)).commits.filter(commitFilter)
-
-    if(commitsWithMetaData.length) {
-      return commitsWithMetaData.map(async commit => {
-       const path = commit.added[0]
-       const result = await getMetaData(path)
-       const payload = (getDescr[path](atob(result.content)))
-       return setRepoDescription(payload)
-      })
-    }
-  })
-  response.writeHead(200)
-  response.end()
-
+function* getFiles(body) {
+  for (let commit of body.commits) {
+    if (commit.added !== undefined) yield commit
+  }
 }
+async function setData() {
+  console.log(JSON.parse(atob(data)))
+for (let file of getFiles(JSON.parse(atob(data)))) {
+  console.log(file)
+	if (!(file in getDescr)) continue
+
+	let content = await getMetaData(file)
+	let meta = getDescr[file](atob(result.content))
+	setRepoDescription(meta)
+}
+  return setData()
+ response.writeHead(200)
+  response.end()
+}
+// const commitsWithMetaData =JSON.parse(atob(data)).commits.filter(commitFilter)
+
+//     if(commitsWithMetaData.length) {
+//       return commitsWithMetaData.map(async commit => {
+//        const path = commit.added[0]
+//        const result = await getMetaData(path)
+//        const payload = (getDescr[path](atob(result.content)))
+//        return setRepoDescription(payload)
+//       })
+//     }
+//   })
+  // response.writeHead(200)
+  // response.end()
+
+})}
 
 function filterCommits(commits) {
 return commits.filter(obj => obj.added["package.json"])
