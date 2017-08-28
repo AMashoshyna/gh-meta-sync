bcd8cfc0827c9d53c60aa387e3f8a27cfccc4ea7
amashoshyna <a.mashoshyna@gmail.com>
2017-08-29 01:15:35 +0300

    minor fixes

--- test/package.js
+++ test/package.js
@@ -1,24 +1,27 @@
 "use strict"
+
 const nock = require("nock")
-const {json, emit, expect} = require("./helpers")
-const {api, repo, description} = require("./constants")
-const {listen, close} = require("./../server")
+const { json, emit, expect } = require("./helpers")
+const { api, repo, description } = require("./constants")
+const { listen, close } = require("../server")
 
 const path = "package.json"
 const contents = `${repo}/contents/${path}`
 const response =  {
-        commits: [{
-          added: [path, "README.MD"],
-          modified: [],
-        },
-        {
-added:["README.MD"],
-modified: []
-        },
-                {
-added:["README.MD", "package.json"],
-modified: []
-        }],
+        commits: [
+          {
+            added: [path, "README.MD"],
+            modified: [],
+          },
+          {
+            added: ["README.MD"],
+            modified: []
+          },
+          {
+            added:["README.MD", "package.json"],
+            modified: []
+          }
+        ],
       }
 
 describe(path, () => {
@@ -44,7 +47,6 @@ describe(path, () => {
     })
 
     let patchDesc = nock(api)
-      .log(console.log)
       .patch(repo, body => {
         return body.description == description
       })
